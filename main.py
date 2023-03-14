a3517abca2f2b5d2ceb2bb0188c4d53a70277f7c
Aditya MP <211wadityaw112@gmail.com>
2023-03-14 20:34:38 +0700

    update main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
 
 from fastapi import Depends, FastAPI, HTTPException, status
-
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 import crud, schema, models, auth
@@ -29,7 +27,8 @@ async def home():
 
 
 
-@app.post("/users/login", response_model=schema.Token)
+@app.post("/pelajar/login", response_model=schema.Token)
+@app.post("/mentor/login", response_model=schema.Token)
 async def login_for_access_token(form_data: schema.UserLoginForm, db: Session = Depends(get_db)):
     
     user = auth.authenticate_user(db, form_data.email, form_data.password)
@@ -42,25 +41,43 @@ async def login_for_access_token(form_data: schema.UserLoginForm, db: Session = 
     access_token = auth.create_access_token(data={"id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
 
-@app.get("/users/me/", response_model=schema.Mentor)
-async def read_users_me(token_data: schema.TokenData = Depends(auth.get_token_data), db: Session = Depends(get_db)):
+@app.get("/pelajar/mydata/", response_model=schema.Pelajar)
+async def read_users_pelajar(token_data: schema.TokenData = Depends(auth.get_token_data), db: Session = Depends(get_db)):
     """
     kalo mau dipake harus tambahin header "Authorization" (tanpa tanda petik) 
     dengan value "Bearer + (token dari /token)"
-
     """
-    current_user = crud.get_user_mentor_by_id(db, token_data.id)
+    current_user = crud.read_user_pelajar_by_id(db, token_data.id)
     return current_user
 
-@app.post("/users/register/mentor")
-async def register_account_mentor(mentor_register_form: schema.MentorRegisterForm, db: Session = Depends(get_db)):
+@app.get("/mentor/mydata/", response_model=schema.Mentor)
+async def read_users_mentor(token_data: schema.TokenData = Depends(auth.get_token_data), db: Session = Depends(get_db)):
+    """
+    kalo mau dipake harus tambahin header "Authorization" (tanpa tanda petik) 
+    dengan value "Bearer + (token dari /token)"
+    """
+    current_user = crud.read_user_mentor_by_id(db, token_data.id)
+    return current_user
+
+@app.post("/pelajar/register/")
+async def register_account_mentor(register_form: schema.PelajarRegisterForm, db: Session = Depends(get_db)):
+    """
+    Membuat akun pelajar baru
+    """
+    
+    crud.create_user_pelajar(db,register_form);
+    return {"details":"ok"}
+
+@app.post("/mentor/register/")
+async def register_account_mentor(register_form: schema.MentorRegisterForm, db: Session = Depends(get_db)):
     """
     Membuat akun mentor baru
     """
     
-    crud.create_user_mentor(db, mentor_register_form);
-    
-    return {"message":"ok"}
+    crud.create_user_mentor(db,register_form);
+    return {"details":"ok"}
+
+
 
 
 
